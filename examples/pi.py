8b4ed94821735501513e24df9d169ad984981934
tasson <christine.tasson@lip6.fr>
2024-04-05 23:35:21 +0200

    merge pi

--- examples/pi.py
+++ examples/pi.py
@@ -31,3 +31,20 @@ with ImportanceSampling(num_particles=100):
     mean, std = dist2.stats()
     viz(dist2)
     plt.show()
+
+
+# def disk() -> Tuple[float, float]:
+#     x = sample(Uniform(-1, 1))
+#     y = sample(Uniform(-1, 1))
+#     d = x**2 + y**2
+#     assume(d < 1)
+#     return (x, y)
+
+
+# with RejectionSampling(num_samples=1000):
+#     dist: Empirical[Tuple[float, float]] = infer(disk)  # type: ignore
+#     x, y = zip(*dist.samples)
+#     sns.scatterplot(x=x, y=y)
+#     plt.axis("scaled")
+#     plt.show()
+
